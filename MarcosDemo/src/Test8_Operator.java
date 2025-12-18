5e4acef07a5ae882e98941e013fb27364698bde1
ParaHuang <paradox041@gmail.com>
2025-12-18 19:53:32 +0800

    operators

---
+++ MarcosDemo/src/Test8_Operator.java
@@ -0,0 +1,22 @@
+public class Test8_Operator {
+    public static void main(String[] args) {
+        int radius = 8;
+//        the radius of circle is ??
+//        System.out.printf("the radius of circle is %d",radius);
+
+        System.out.println("the radius of circle is " + radius);
+//        System.out.println(radius);
+        //println   ->  print line
+
+        // the use of +
+        System.out.println(3+4);
+//        System.out.println("normal content"+variable);
+        System.out.println("hello"+radius);
+
+        // number1 + number2    ->      number      ->this + is doing math operation
+        // "content" + 4        ->      content4    ->this + is connecting those together
+
+        System.out.println("the radius of circle is "+radius);
+
+    }
+}
